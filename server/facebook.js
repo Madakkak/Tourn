656273054972841ed200f474012988b3a47d5d86
Maher Dakkak <madakkak@gmail.com>
2016-09-07 19:46:30 -0500

    Clean up Passport Facebook authentication code.

--- server/facebook.js
+++ server/facebook.js
@@ -40,62 +40,61 @@ passport.deserializeUser((obj, done) => {
 });
 
 
-//***************Facebook Authentication Routing********************
+// ***************Facebook Authentication Routing********************
 
 passport.use(new FacebookStrategy({
-    clientID: 'CLIENT_ID',
-    clientSecret: 'SECRET_KEY',
-    callbackURL: "http://localhost:4000/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    var _id = profile.id;
-    console.log("accessToken:", accessToken);
-    // console.log("refreshToken:", refreshToken);
-    console.log("profile:", profile.displayName);
-    // console.log("done:", done);
-    console.log("id:", _id);
+  clientID: 'CLIENT_ID',
+  clientSecret: 'SECRET_KEY',
+  callbackURL: 'http://localhost:4000/auth/facebook/callback',
+}, (accessToken, refreshToken, profile, done) => {
+  const _id = profile.id;
+  console.log('accessToken:', accessToken);
+  // console.log("refreshToken:", refreshToken);
+  console.log('profile:', profile.displayName);
+  // console.log("done:", done);
+  console.log('id:', _id);
 
-    //
-    // Functions below are exampleS of calling server models here.
-    //
+  //
+  // Functions below are exampleS of calling server models here.
+  //
 
-    // User.findById(_id)
-    // .then(function(response){
-    //   console.log("userRes:", response);
-    // })
-    // .catch(function(error){
-    //   console.error("userResErr: ", error);
-    // })
-    //
-    // User.create(_id, profile.displayName)
-    // .then(function(user){
-    //   console.log("newUserRes:", user);
-    //   return done(null, user);
-    // })
-    // .catch(function(error){
-    //   return done(error);
-    // })
-    return done(null, 'login worked');
-  }
+  // User.findById(_id)
+  // .then(function(response){
+  //   console.log("userRes:", response);
+  // })
+  // .catch(function(error){
+  //   console.error("userResErr: ", error);
+  // })
+  //
+  // User.create(_id, profile.displayName)
+  // .then(function(user){
+  //   console.log("newUserRes:", user);
+  //   return done(null, user);
+  // })
+  // .catch(function(error){
+  //   return done(error);
+  // })
+  return done(null, 'login worked');
+}
 ));
 
 // Redirect the user to Facebook for authentication.  When complete,
 // Facebook will redirect the user back to the application at
 //     /auth/facebook/callback
-app.get('/auth/facebook', passport.authenticate('facebook'), function(req, res){
+app.get('/auth/facebook', passport.authenticate('facebook'), (req, res) => {
 	// available callback function
 });
 
 // Facebook will redirect the user to this URL after approval.  Finish the
-// authentication process by attempting to obtain an access token.  If
+// authentication process by attempting to obtain an access token. If
 // access was granted, the user will be logged in.  Otherwise,
 // authentication has failed.
 app.get('/auth/facebook/callback',
   passport.authenticate('facebook', {
-  	failureRedirect: '/login'
-  }),
-  function(req, res){
+    failureRedirect: '/login',
+  }), (req, res) => {
     res.redirect('/');
   }
 );
-//******************************************************************
+
+// ******************************************************************
